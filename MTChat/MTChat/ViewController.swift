5d5b88989532032af9cb24eb986d63c6447db270
MQL-IT <mantaios@ITdeMacBook-Pro.local>
2018-11-26 15:38:17 +0800

    创建项目

---
+++ MTChat/MTChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MTChat
+//
+//  Created by IT A on 2018/11/26.
+//  Copyright © 2018 IT A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
